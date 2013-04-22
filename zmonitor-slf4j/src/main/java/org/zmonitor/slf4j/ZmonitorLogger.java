6be4f0c69edbf66a16125e078d35a58280e21701
Ian Tsai <zanyking@gmail.com>
2013-04-22 10:46:18 +0800

    add slf4j support

---
+++ zmonitor-slf4j/src/main/java/org/zmonitor/slf4j/ZmonitorLogger.java
@@ -0,0 +1,176 @@
+/**
+ * 
+ */
+package org.zmonitor.slf4j;
+
+import org.slf4j.helpers.MarkerIgnoringBase;
+
+/**
+ * 
+ * Zmonitor implementation of Logger that sends all enabled log messages, for all defined loggers, to Zmonitor.
+ * 
+ * 
+ * @author Ian YT Tsai(Zanyking)
+ *
+ */
+public class ZmonitorLogger extends MarkerIgnoringBase {
+	private static final long serialVersionUID = -5063731853235642189L;
+
+	
+	
+	
+	
+	
+	public boolean isTraceEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void trace(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void trace(String format, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void trace(String format, Object arg1, Object arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void trace(String format, Object... arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void trace(String msg, Throwable t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isDebugEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void debug(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void debug(String format, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void debug(String format, Object arg1, Object arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void debug(String format, Object... arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void debug(String msg, Throwable t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isInfoEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void info(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String format, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String format, Object arg1, Object arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String format, Object... arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void info(String msg, Throwable t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isWarnEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void warn(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void warn(String format, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void warn(String format, Object... arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void warn(String format, Object arg1, Object arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void warn(String msg, Throwable t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean isErrorEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void error(String msg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void error(String format, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void error(String format, Object arg1, Object arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void error(String format, Object... arguments) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void error(String msg, Throwable t) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
